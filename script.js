0091d159e5742ea83ac152664cf1d555574a0a5f
eryk0004 <eryk0004@stud.kea.dk>
2021-10-13 11:45:53 +0200

    Second

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
   const h2 = section.querySelector("h2");
   const p = section.querySelectorAll("p");
-  const imgIcons = section.querySelectorAll(".imgIcons");
+  const img = section.querySelectorAll("img");
   gsap
     .timeline({
       scrollTrigger: {
         trigger: section,
-        start: "400% 400%",
+        start: "top 50%",
         end: "bottom 100%",
-        scrub: true,
+        scrub: 1,
       },
     })
     .from(h2, {
+      duration: 3,
+      opacity: 0,
+    })
+    .from(img, {
       duration: 3,
       opacity: 0,
     })
